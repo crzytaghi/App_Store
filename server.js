98d4b9254f6abff66973245a6a21fe5099952f21
Andrew Taghi <aataghi13@gmail.com>
2020-09-22 17:38:41 -0500

    updated path

--- server.js
+++ server.js
@@ -32,10 +32,10 @@ app.get('/products/:id', (req,res,next) => {
 // Serve static assets if in production
 if(process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('app_store/build'));
+  app.use(express.static('client/build'));
 
   app.get('*', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'app_store', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   })
 }
 
